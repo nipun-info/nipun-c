33a59ac59712d7b4dd33df1a579ca7510642c742
nipun <nipun.info2020@gmail.com>
2024-10-22 08:47:26 +0600

    char problem lowercase to uppercase letter solve

---
+++ problemSolve/13_char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char x;
+
+    scanf("%c", &x);
+
+    if (x >= 'a' && x <= 'z')
+    {
+        x = x - 32;
+    }
+    else
+    {
+        x = x + 32;
+    }
+
+    printf("%c\n", x);
+
+    return 0;
+}
